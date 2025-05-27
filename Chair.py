d004a5ec930e6f2c8eaa8cdaa46292b2b1077dd4
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-05-27 23:35:15 +0800

    Delete Chair.py

--- Chair.py
+++
@@ -1,7 +0,0 @@
-# Concrete Element 1
-class Chair(Furniture):
-    def __init__(self, material):
-        self.material = material
-
-    def accept(self, visitor):
-        return visitor.visit_chair(self)
