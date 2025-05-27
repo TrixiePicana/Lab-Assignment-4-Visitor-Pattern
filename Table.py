f521a6a78ebbd8c7da29e01b86e4a71265c8921e
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-05-27 23:37:46 +0800

    Delete Table.py

--- Table.py
+++
@@ -1,7 +0,0 @@
-# Concrete Element 2
-class Table(Furniture):
-    def __init__(self, weight):
-        self.weight = weight #in kg
-
-    def accept(self, visitor):
-        return visitor.visit_table(self)
