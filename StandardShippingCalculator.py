61cf5700fe6487cfc7c8fb41a980934fe359ad90
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-05-30 00:28:41 +0800

    Update StandardShippingCalculator.py

--- StandardShippingCalculator.py
+++ StandardShippingCalculator.py
@@ -1,23 +1,15 @@
-# Concrete Visitor Class
-import ShippingCostCalculator
-
-
-class StandardShippingCalculator(ShippingCostCalculator):
+class StandardShippingCalculator:
     def visit_chair(self, chair):
-        # Shipping cost based on material
+        # Example logic
         if chair.material == "wood":
             return 20
-        
         elif chair.material == "metal":
             return 30
         else:
             return 15
-    
+
     def visit_table(self, table):
-        # Shipping cost based on weight
-        return 10 + 0.5 * table.weight # $10 base + $0.5/kg
-    
+        return 10 + 0.5 * table.weight
+
     def visit_sofa(self, sofa):
-        # shipping cost based on volume and distance
         return 50 + 2 * sofa.volume + 0.1 * sofa.distance
-        # 50 is the base cost, 2 is the cost per cubic meter, and 0.1 is the cost per km
