9b4f6705508d013dc4539b6f8d846bb891fca2ea
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-05-29 23:10:16 +0800

    Update Sofa.py

--- Sofa.py
+++ Sofa.py
@@ -1,11 +1,10 @@
 # Concrete Element 3
-import Furniture
-
+from Furniture import Furniture
 
 class Sofa(Furniture):
     def __init__(self, volume, distance):
         self.volume = volume # in cubic meters
         self.distance = distance # in km
-
+        
     def accept(self, visitor):
         return visitor.visit_sofa(self)
